37fb0dd1ec73fb5c1f83d97fe0e897049658216e
Achal123Eminence <achalmudgaleminenceinnovation@gmail.com>
2026-09-04 16:52:57 +0530

    UI done to look advanced

--- Frontend/src/app/shared/header/header.ts
+++ Frontend/src/app/shared/header/header.ts
@@ -1,15 +1,45 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
   standalone: true
 })
 export class Header {
+
   private router = inject(Router);
+
+  username = '';
+  role = '';
+
+  ngOnInit() {
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+
+      this.username = user.username || '';
+      this.role = this.getRoleName(user.level);
+    }
+  }
+
+  private getRoleName(level: number): string {
+    switch (level) {
+      case 1:
+        return 'Owner';
+      case 2:
+        return 'Sub Admin';
+      case 3:
+        return 'Admin';
+      case 4:
+        return 'Agent';
+      default:
+        return 'User';
+    }
+  }
 
   logout() {
     localStorage.removeItem('accessToken');
@@ -17,4 +47,4 @@ export class Header {
 
     this.router.navigate(['/login']);
   }
-};
+}
